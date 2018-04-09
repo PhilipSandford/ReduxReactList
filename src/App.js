6a6ec4b034b0b6438cdd2667876de34f9fa485ae
Philip <ps451@kent.ac.uk>
2018-04-09 11:19:48 +0100

    Remove react logo

--- src/App.js
+++ src/App.js
@@ -13,8 +13,7 @@ class App extends Component {
       <Provider store={store}>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Redux/React</h1>
         </header>
         <Postform />
         <hr />
